8a28c4f95f352ebacede0cb2f452d788b1d9de45
krasninja <fwd2ivan@gmail.com>
2016-06-23 16:50:17 +0700

    fixed ruleset install script encoding

--- src/Saritasa.Tools/SaritasaRulesetInstall.ps1
+++ src/Saritasa.Tools/SaritasaRulesetInstall.ps1
@@ -1,6 +1,5 @@
-﻿# Installs Saritasa stylecop analyzer ruleset
-#
+# Installs Saritasa stylecop analyzer ruleset
 
 Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/krasninja/SaritasaTools/develop/src/Saritasa.ruleset' `
     -OutFile "${env:ProgramFiles(x86)}\Microsoft Visual Studio 14.0\Team Tools\Static Analysis Tools\Rule Sets\Saritasa.ruleset"
-Write-Host 'The Saritasa C# ruleset is successfully installed!'
+Write-Output 'The Saritasa C# ruleset is successfully installed!'
