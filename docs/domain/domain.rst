ec9e8110b1f068808f99aaeb88115ac67294c09d
krasninja <fwd2ivan@gmail.com>
2016-11-04 17:40:14 +0700

    Add SecurityException

--- docs/domain/domain.rst
+++ docs/domain/domain.rst
@@ -10,6 +10,8 @@ Exceptions
 
 ``NotFoundException`` - Inherits from ``DomainException``. Usually means that object you try to find by specified key is not in collection.
 
+``SecurityException`` - Inherits from ``DomainException``. Means that user cannot access to any resource or do any operation because he has no permissions.
+
 Domain Events
 -------------
 
---
+++ src/Saritasa.Tools.Domain/Exceptions/SecurityException.cs
@@ -0,0 +1,51 @@
+﻿// Copyright (c) 2015-2016, Saritasa. All rights reserved.
+// Licensed under the BSD license. See LICENSE file in the project root for full license information.
+
+namespace Saritasa.Tools.Domain.Exceptions
+{
+    using System;
+    using System.Runtime.Serialization;
+
+    /// <summary>
+    /// Domain security exception.
+    /// </summary>
+#if !NETCOREAPP1_0 && !NETSTANDARD1_6
+    [Serializable]
+#endif
+    public class SecurityException : DomainException
+    {
+        /// <summary>
+        /// .ctor
+        /// </summary>
+        public SecurityException() : base("Permissions required")
+        {
+        }
+
+        /// <summary>
+        /// .ctor
+        /// </summary>
+        public SecurityException(string message) : base(message)
+        {
+        }
+
+        /// <summary>
+        /// .ctor
+        /// </summary>
+        public SecurityException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+#if !NETCOREAPP1_0 && !NETSTANDARD1_6
+        /// <summary>
+        /// .ctor for deserialization.
+        /// </summary>
+        /// <param name="info">Stores all the data needed to serialize or deserialize an object.</param>
+        /// <param name="context">Describes the source and destination of a given serialized stream,
+        /// and provides an additional caller-defined context.</param>
+        protected SecurityException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+#endif
+    }
+}
