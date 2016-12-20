e0f6e74100f96b6bc4924fd7eeb7126babd44fb5
krasninja <fwd2ivan@gmail.com>
2016-12-20 18:23:38 +0700

    Queue to SmtpClientEmailSender, add MultiSmtpClientEmailSender

---
+++ src/Saritasa.Tools.Emails/EmailQueueExceededException.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) 2015-2016, Saritasa. All rights reserved.
+// Licensed under the BSD license. See LICENSE file in the project root for full license information.
+
+namespace Saritasa.Tools.Emails
+{
+    using System;
+
+    /// <summary>
+    /// Exception occures when email sending queue is overloaded.
+    /// </summary>
+    public class EmailQueueExceededException : Exception
+    {
+        /// <summary>
+        /// .ctor
+        /// </summary>
+        /// <param name="maxSize">Max queue size that has benn exceeded.</param>
+        public EmailQueueExceededException(int maxSize) :
+            base($"Maximum email queue size {maxSize} exceeded.")
+        {
+        }
+    }
+}
--- src/Saritasa.Tools.Emails/SmtpClientEmailSender.cs
+++ src/Saritasa.Tools.Emails/SmtpClientEmailSender.cs
@@ -5,19 +5,65 @@
 namespace Saritasa.Tools.Emails
 {
     using System;
+    using System.Collections.Concurrent;
     using System.Collections.Generic;
+    using System.ComponentModel;
     using System.Net.Mail;
     using System.Threading.Tasks;
 
     /// <summary>
-    /// Send email using SmtpClient.
+    /// Send email using SmtpClient. The class is thread safe and allows multiple calls
+    /// of Send method.
     /// </summary>
     public class SmtpClientEmailSender : EmailSender, IDisposable
     {
         /// <summary>
+        /// We provide another task to client since actual email sending can be delayed
+        /// (enqueued).
+        /// </summary>
+        struct MailMessageWithTaskSource
+        {
+            /// <summary>
+            /// Completion source for task.
+            /// </summary>
+            public TaskCompletionSource<int> TaskCompletionSource { get; }
+
+            /// <summary>
+            /// Mail message.
+            /// </summary>
+            public MailMessage MailMessage { get; }
+
+            /// <summary>
+            /// .ctor
+            /// </summary>
+            /// <param name="mailMessage">Mail message.</param>
+            public MailMessageWithTaskSource(MailMessage mailMessage)
+            {
+                MailMessage = mailMessage;
+                TaskCompletionSource = new TaskCompletionSource<int>();
+            }
+        }
+
+        readonly ConcurrentQueue<MailMessageWithTaskSource> queue =
+            new ConcurrentQueue<MailMessageWithTaskSource>();
+
+        /// <summary>
+        /// Indicates the email message is currently sending.
+        /// </summary>
+        bool isBusy;
+
+        readonly object @lock = new object();
+
+        /// <summary>
         /// Instance of SmtpClient.
         /// </summary>
         public SmtpClient Client { get; }
+
+        /// <summary>
+        /// Maximum queue size. If queue size for some reason is exceeded the
+        /// <see cref="EmailQueueExceededException" /> exception will be thrown.
+        /// </summary>
+        public int MaxQueueSize = 10240;
 
         /// <summary>
         /// .ctor
@@ -25,6 +71,7 @@ namespace Saritasa.Tools.Emails
         public SmtpClientEmailSender()
         {
             Client = new SmtpClient();
+            Client.SendCompleted += OnEmailSent;
         }
 
         /// <summary>
@@ -38,13 +85,79 @@ namespace Saritasa.Tools.Emails
                 throw new ArgumentNullException(nameof(smtpClient));
             }
 
-            Client = new SmtpClient();
+            Client = smtpClient;
+            Client.SendCompleted += OnEmailSent;
+        }
+
+        private void OnEmailSent(object sender, AsyncCompletedEventArgs args)
+        {
+            // the callback is called before the task
+            isBusy = false;
+            ProcessInternal();
         }
 
         /// <inheritdoc />
         protected override Task Process(MailMessage message, IDictionary<string, object> data)
         {
-            return Client.SendMailAsync(message);
+            if (disposed)
+            {
+                throw new ObjectDisposedException(null);
+            }
+
+            var messageTask = new MailMessageWithTaskSource(message);
+            queue.Enqueue(messageTask);
+            if (queue.Count > MaxQueueSize)
+            {
+                throw new EmailQueueExceededException(MaxQueueSize);
+            }
+            ProcessInternal();
+            return messageTask.TaskCompletionSource.Task;
+        }
+
+        /// <summary>
+        /// Calls email processing method directly skipping all interceptors.
+        /// </summary>
+        /// <param name="message">Mail message.</param>
+        /// <param name="data">Additional data.</param>
+        /// <returns>Async task operation.</returns>
+        internal Task SendAsyncInternal(MailMessage message, IDictionary<string, object> data)
+        {
+            return Process(message, data);
+        }
+
+        private void ProcessInternal()
+        {
+            lock (@lock)
+            {
+                if (isBusy)
+                {
+                    return;
+                }
+
+                MailMessageWithTaskSource messageTask;
+                if (queue.TryDequeue(out messageTask))
+                {
+                    isBusy = true;
+                    Client.SendMailAsync(messageTask.MailMessage).ContinueWith(t =>
+                    {
+                        isBusy = false;
+
+                        // sync current task status (from email) with one that is waited by user
+                        if (t.IsFaulted && t.Exception?.InnerExceptions != null)
+                        {
+                            messageTask.TaskCompletionSource.SetException(t.Exception.InnerExceptions);
+                        }
+                        else if (t.IsCanceled)
+                        {
+                            messageTask.TaskCompletionSource.SetCanceled();
+                        }
+                        else if (t.IsCompleted)
+                        {
+                            messageTask.TaskCompletionSource.SetResult(0);
+                        }
+                    });
+                }
+            }
         }
 
         #region Dispose
