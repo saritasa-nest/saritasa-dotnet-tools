7e7120da4b4cb29e67583e79d9df0bb986712022
krasninja <fwd2ivan@gmail.com>
2016-12-23 10:17:47 +0700

    IEmailSender.Send extension method

--- src/Saritasa.Tools.Messages.Abstractions/IMessage.cs
+++ src/Saritasa.Tools.Messages.Abstractions/IMessage.cs
@@ -1,11 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// Copyright (c) 2015-2016, Saritasa. All rights reserved.
+// Licensed under the BSD license. See LICENSE file in the project root for full license information.
 
 namespace Saritasa.Tools.Messages.Abstractions
 {
+    using System;
+    using System.Collections.Generic;
+
     /// <summary>
     /// Abstract message.
     /// </summary>
--- src/Saritasa.Tools.Messages.Abstractions/ProcessingStatus.cs
+++ src/Saritasa.Tools.Messages.Abstractions/ProcessingStatus.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// Copyright (c) 2015-2016, Saritasa. All rights reserved.
+// Licensed under the BSD license. See LICENSE file in the project root for full license information.
 
 namespace Saritasa.Tools.Messages.Abstractions
 {
