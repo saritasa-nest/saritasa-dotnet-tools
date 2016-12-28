da8ed7619f34bee58c66498dcf272e58265270a4
krasninja <fwd2ivan@gmail.com>
2016-12-28 12:05:29 +0700

    Update Stylecop rules, update ruleset install script

--- src/SaritasaRulesetInstall.ps1
+++
@@ -1,5 +0,0 @@
-# Installs Saritasa stylecop analyzer ruleset
-
-Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/saritasa/SaritasaTools/develop/src/tools/Saritasa.ruleset' `
-    -OutFile "${env:ProgramFiles(x86)}\Microsoft Visual Studio 14.0\Team Tools\Static Analysis Tools\Rule Sets\Saritasa.ruleset"
-Write-Output 'The Saritasa C# ruleset is successfully installed!'
