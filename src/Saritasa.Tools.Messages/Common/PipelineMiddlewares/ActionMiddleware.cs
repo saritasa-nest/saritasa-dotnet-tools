aa5b479acf5b6eab255be8e1294e911042fe186d
krasninja <fwd2ivan@gmail.com>
2017-07-16 20:22:45 +0700

    Add WebService repository and Action call middleware

---
+++ src/Saritasa.Tools.Messages/Common/PipelineMiddlewares/ActionMiddleware.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) 2015-2017, Saritasa. All rights reserved.
+// Licensed under the BSD license. See LICENSE file in the project root for full license information.
+
+using System;
+using Saritasa.Tools.Messages.Abstractions;
+
+namespace Saritasa.Tools.Messages.Common.PipelineMiddlewares
+{
+    /// <summary>
+    /// Execute user action on every message.
+    /// </summary>
+    public class ActionMiddleware : IMessagePipelineMiddleware
+    {
+        /// <inheritdoc />
+        public string Id { get; set; } = "Action";
+
+        readonly Action<IMessage> action;
+
+        /// <summary>
+        /// .ctor
+        /// </summary>
+        /// <param name="action">The action to be executed.</param>
+        public ActionMiddleware(Action<IMessage> action)
+        {
+            if (action == null)
+            {
+                throw new ArgumentNullException(nameof(action));
+            }
+            this.action = action;
+        }
+
+        /// <inheritdoc />
+        public virtual void Handle(IMessage context)
+        {
+            action(context);
+        }
+    }
+}
---
+++ src/Saritasa.Tools.Messages/Common/Repositories/WebServiceRepository.cs
@@ -0,0 +1,130 @@
+﻿// Copyright (c) 2015-2017, Saritasa. All rights reserved.
+// Licensed under the BSD license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Saritasa.Tools.Messages.Abstractions;
+using Saritasa.Tools.Messages.Common.ObjectSerializers;
+using Saritasa.Tools.Messages.Internal;
+
+namespace Saritasa.Tools.Messages.Common.Repositories
+{
+    /// <summary>
+    /// Send or get messages from remote web endpoint.
+    /// </summary>
+    public class WebServiceRepository : IMessageRepository
+    {
+        private const string KeyUri = "uri";
+        private const string DefaultEndpoint = @"http://localhost:9990/";
+
+        private HttpClient client = new HttpClient();
+        private readonly string uri;
+        private readonly JsonObjectSerializer serializer = new JsonObjectSerializer();
+
+        /// <summary>
+        /// .ctor
+        /// </summary>
+        public WebServiceRepository()
+        {
+            this.uri = DefaultEndpoint;
+            Init();
+        }
+
+        /// <summary>
+        /// .ctor
+        /// </summary>
+        /// <param name="uri">Uri to send messages.</param>
+        public WebServiceRepository(string uri)
+        {
+            if (string.IsNullOrWhiteSpace(uri))
+            {
+                throw new ArgumentNullException(nameof(uri));
+            }
+            this.uri = uri;
+            Init();
+        }
+
+        /// <summary>
+        /// .ctor
+        /// </summary>
+        /// <param name="parameters">Parameters dictionary.</param>
+        public WebServiceRepository([NotNull] IDictionary<string, string> parameters)
+        {
+            this.uri = parameters.GetValueOrInvoke(KeyUri, RepositoryConfigurationException.ThrowParameterNotExists);
+            Init();
+        }
+
+        private void Init()
+        {
+            //client.Timeout = TimeSpan.FromSeconds(5);
+        }
+
+        #region IMessageRepository
+
+        /// <inheritdoc />
+        public async Task AddAsync([NotNull] IMessage message, CancellationToken cancellationToken)
+        {
+            if (disposed)
+            {
+                throw new ObjectDisposedException(null);
+            }
+
+            var content = Encoding.UTF8.GetString(serializer.Serialize(((Message)message).CloneToMessage()));
+            await client.PostAsync(
+                uri,
+                new StringContent(content, Encoding.UTF8, "application/json"),
+                cancellationToken).ConfigureAwait(false);
+        }
+
+        /// <inheritdoc />
+        public async Task<IEnumerable<IMessage>> GetAsync([NotNull] MessageQuery messageQuery, CancellationToken cancellationToken)
+        {
+            if (disposed)
+            {
+                throw new ObjectDisposedException(null);
+            }
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public void SaveState([NotNull] IDictionary<string, string> parameters)
+        {
+            parameters[KeyUri] = uri;
+        }
+
+        #endregion
+
+        #region Dispose
+
+        bool disposed;
+
+        /// <inheritdoc />
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    client.Dispose();
+                    client = null;
+                }
+                disposed = true;
+            }
+        }
+
+        /// <inheritdoc />
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        #endregion
+    }
+}
