71be529973a56abc77539697d93b06daa1566e0f
Anton Zimin <anton@saritasa.com>
2017-11-10 18:14:17 +0700

    Introduce publish-zergrushco and publish-boringwarehouse tasks, copy scripts into container #30

--- scripts/PublishTasks.ps1
+++ scripts/PublishTasks.ps1
@@ -16,3 +16,30 @@ Task package-boringwarehouse -depends pre-build `
     Invoke-PackageBuild -ProjectPath "$samples\Saritasa.BoringWarehouse\Saritasa.BoringWarehouse.Web\Saritasa.BoringWarehouse.Web.csproj" `
         -PackagePath $packagePath -Configuration $Configuration
 }
+
+function DeployLocalProject([string] $packagePath, [string] $connectionString, [string[]] $params)
+{
+    $allParams = @('-verb:sync',
+        "-source:package='$packagePath'",
+        '-dest:auto',
+        "-setParam:name='IIS Web Application Name',value='Default Web Site/'",
+        "-setParam:name='AppDbContext-Web.config Connection String',value='$connectionString'")
+
+    if ($params)
+    {
+        $allParams += $params
+    }
+
+    Start-Process -Wait -NoNewWindow -PassThru 'C:\Program Files\IIS\Microsoft Web Deploy V3\msdeploy.exe' $allParams
+}
+
+Task publish-zergrushco `
+{
+    DeployLocalProject 'ZergRushCo.zip' 'Data Source=zergdb;Initial Catalog=ZergRushCo;User ID=TestUser;Password=gHJT2SCm' `
+        "-setParam:name='AppDbContextProviderName',value='System.Data.SqlClient'"
+}
+
+Task publish-boringwarehouse `
+{
+    DeployLocalProject 'BoringWarehouse.zip' 'Data Source=bwdb;Initial Catalog=BoringWarehouse;User ID=TestUser;Password=gHJT2SCm'
+}
