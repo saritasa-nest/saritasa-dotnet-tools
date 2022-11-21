3d9e8247d4e307a4d8cc03ba4b4eeba268a7851a
Ivan Kozhin <ivan@saritasa.com>
2022-11-21 14:40:13 +0700

    Add ReadOnlySpan overloads for StringUtils

--- src/Saritasa.Tools.Common/Utils/StringUtils.cs
+++ src/Saritasa.Tools.Common/Utils/StringUtils.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) 2015-2021, Saritasa. All rights reserved.
+﻿// Copyright (c) 2015-2022, Saritasa. All rights reserved.
 // Licensed under the BSD license. See LICENSE file in the project root for full license information.
 
 using System;
@@ -92,7 +92,7 @@ namespace Saritasa.Tools.Common.Utils
         /// </summary>
         /// <param name="target">Target string.</param>
         /// <returns>Empty string if null or target string.</returns>
-        public static string NullSafe(string target)
+        public static string NullSafe(string? target)
         {
             return target ?? string.Empty;
         }
@@ -120,7 +120,7 @@ namespace Saritasa.Tools.Common.Utils
             return stringBuilder.ToString();
         }
 
-        #region Parse with default
+        #region Parse or default bool
 
         /// <summary>
         /// Tries to convert the target string to Boolean. If fails return the default value.
@@ -131,6 +131,18 @@ namespace Saritasa.Tools.Common.Utils
             var success = bool.TryParse(target, out result);
             return success ? result : defaultValue;
         }
+
+#if NETCOREAPP3_0_OR_GREATER || NETSTANDARD2_1_OR_GREATER
+        /// <summary>
+        /// Tries to convert the target string to Boolean. If fails return the default value.
+        /// </summary>
+        public static bool ParseOrDefault(ReadOnlySpan<char> target, bool defaultValue)
+        {
+            bool result;
+            var success = bool.TryParse(target, out result);
+            return success ? result : defaultValue;
+        }
+#endif
 
         private static readonly string[] trueValues = { "yes", "on", "y", "t", "1" };
         private static readonly string[] falseValues = { "no", "off", "n", "f", "0" };
@@ -160,6 +172,10 @@ namespace Saritasa.Tools.Common.Utils
             return success ? result : defaultValue;
         }
 
+        #endregion
+
+        #region Parse or default byte
+
         /// <summary>
         /// Tries to convert the target string to Byte. If fails return the default value.
         /// </summary>
@@ -180,6 +196,32 @@ namespace Saritasa.Tools.Common.Utils
             return success ? result : defaultValue;
         }
 
+#if NETCOREAPP3_0_OR_GREATER || NETSTANDARD2_1_OR_GREATER
+        /// <summary>
+        /// Tries to convert the target string to Byte. If fails return the default value.
+        /// </summary>
+        public static byte ParseOrDefault(ReadOnlySpan<char> target, byte defaultValue)
+        {
+            byte result;
+            var success = byte.TryParse(target, out result);
+            return success ? result : defaultValue;
+        }
+
+        /// <summary>
+        /// Tries to convert the target string to Byte. If fails return the default value.
+        /// </summary>
+        public static byte ParseOrDefault(ReadOnlySpan<char> target, NumberStyles style, IFormatProvider provider, byte defaultValue)
+        {
+            byte result;
+            var success = byte.TryParse(target, style, provider, out result);
+            return success ? result : defaultValue;
+        }
+#endif
+
+        #endregion
+
+        #region Parse or default char
+
         /// <summary>
         /// Tries to convert the target string to Char. If fails return the default value.
         /// </summary>
@@ -189,6 +231,10 @@ namespace Saritasa.Tools.Common.Utils
             var success = char.TryParse(target, out result);
             return success ? result : defaultValue;
         }
+
+        #endregion
+
+        #region Parse or default date time
 
         /// <summary>
         /// Tries to convert the target string to DateTime. If fails return the default value.
@@ -200,6 +246,18 @@ namespace Saritasa.Tools.Common.Utils
             return success ? result : defaultValue;
         }
 
+#if NETCOREAPP3_0_OR_GREATER || NETSTANDARD2_1_OR_GREATER
+        /// <summary>
+        /// Tries to convert the target string to DateTime. If fails return the default value.
+        /// </summary>
+        public static DateTime ParseOrDefault(ReadOnlySpan<char> target, DateTime defaultValue)
+        {
+            DateTime result;
+            var success = DateTime.TryParse(target, out result);
+            return success ? result : defaultValue;
+        }
+#endif
+
         /// <summary>
         /// Tries to convert the target string to DateTime. If fails return the default value.
         /// </summary>
@@ -209,6 +267,22 @@ namespace Saritasa.Tools.Common.Utils
             var success = DateTime.TryParse(target, provider, styles, out result);
             return success ? result : defaultValue;
         }
+
+#if NETCOREAPP3_0_OR_GREATER || NETSTANDARD2_1_OR_GREATER
+        /// <summary>
+        /// Tries to convert the target string to DateTime. If fails return the default value.
+        /// </summary>
+        public static DateTime ParseOrDefault(ReadOnlySpan<char> target, IFormatProvider provider, DateTimeStyles styles, DateTime defaultValue)
+        {
+            DateTime result;
+            var success = DateTime.TryParse(target, provider, styles, out result);
+            return success ? result : defaultValue;
+        }
+#endif
+
+        #endregion
+
+        #region Parse or default decimal
 
         /// <summary>
         /// Tries to convert the target string to Decimal. If fails return the default value.
@@ -230,6 +304,32 @@ namespace Saritasa.Tools.Common.Utils
             return success ? result : defaultValue;
         }
 
+#if NETCOREAPP3_0_OR_GREATER || NETSTANDARD2_1_OR_GREATER
+        /// <summary>
+        /// Tries to convert the target string to Decimal. If fails return the default value.
+        /// </summary>
+        public static decimal ParseOrDefault(ReadOnlySpan<char> target, decimal defaultValue)
+        {
+            decimal result;
+            var success = decimal.TryParse(target, out result);
+            return success ? result : defaultValue;
+        }
+
+        /// <summary>
+        /// Tries to convert the target string to Decimal. If fails return the default value.
+        /// </summary>
+        public static decimal ParseOrDefault(ReadOnlySpan<char> target, NumberStyles style, IFormatProvider provider, decimal defaultValue)
+        {
+            decimal result;
+            var success = decimal.TryParse(target, style, provider, out result);
+            return success ? result : defaultValue;
+        }
+#endif
+
+        #endregion
+
+        #region Parse or default double
+
         /// <summary>
         /// Tries to convert the target string to Double. If fails return the default value.
         /// </summary>
@@ -249,6 +349,32 @@ namespace Saritasa.Tools.Common.Utils
             var success = double.TryParse(target, style, provider, out result);
             return success ? result : defaultValue;
         }
+
+#if NETCOREAPP3_0_OR_GREATER || NETSTANDARD2_1_OR_GREATER
+        /// <summary>
+        /// Tries to convert the target string to Double. If fails return the default value.
+        /// </summary>
+        public static double ParseOrDefault(ReadOnlySpan<char> target, double defaultValue)
+        {
+            double result;
+            var success = double.TryParse(target, out result);
+            return success ? result : defaultValue;
+        }
+
+        /// <summary>
+        /// Tries to convert the target string to Double. If fails return the default value.
+        /// </summary>
+        public static double ParseOrDefault(ReadOnlySpan<char> target, NumberStyles style, IFormatProvider provider, double defaultValue)
+        {
+            double result;
+            var success = double.TryParse(target, style, provider, out result);
+            return success ? result : defaultValue;
+        }
+#endif
+
+        #endregion
+
+        #region Parse or default short
 
         /// <summary>
         /// Tries to convert the target string to Int16. If fails return the default value.
@@ -270,6 +396,32 @@ namespace Saritasa.Tools.Common.Utils
             return success ? result : defaultValue;
         }
 
+#if NETCOREAPP3_0_OR_GREATER || NETSTANDARD2_1_OR_GREATER
+        /// <summary>
+        /// Tries to convert the target string to Int16. If fails return the default value.
+        /// </summary>
+        public static short ParseOrDefault(ReadOnlySpan<char> target, short defaultValue)
+        {
+            short result;
+            var success = short.TryParse(target, out result);
+            return success ? result : defaultValue;
+        }
+
+        /// <summary>
+        /// Tries to convert the target string to Int16. If fails return the default value.
+        /// </summary>
+        public static short ParseOrDefault(ReadOnlySpan<char> target, NumberStyles style, IFormatProvider provider, short defaultValue)
+        {
+            short result;
+            var success = short.TryParse(target, style, provider, out result);
+            return success ? result : defaultValue;
+        }
+#endif
+
+        #endregion
+
+        #region Parse or default int
+
         /// <summary>
         /// Tries to convert the target string to int. If fails return the default value.
         /// </summary>
@@ -290,6 +442,32 @@ namespace Saritasa.Tools.Common.Utils
             return success ? result : defaultValue;
         }
 
+#if NETCOREAPP3_0_OR_GREATER || NETSTANDARD2_1_OR_GREATER
+        /// <summary>
+        /// Tries to convert the target string to int. If fails return the default value.
+        /// </summary>
+        public static int ParseOrDefault(ReadOnlySpan<char> target, int defaultValue)
+        {
+            int result;
+            var success = int.TryParse(target, out result);
+            return success ? result : defaultValue;
+        }
+
+        /// <summary>
+        /// Tries to convert the target string to int. If fails return the default value.
+        /// </summary>
+        public static int ParseOrDefault(ReadOnlySpan<char> target, NumberStyles style, IFormatProvider provider, int defaultValue)
+        {
+            int result;
+            var success = int.TryParse(target, style, provider, out result);
+            return success ? result : defaultValue;
+        }
+#endif
+
+        #endregion
+
+        #region Parse or default long
+
         /// <summary>
         /// Tries to convert the target string to Int64. If fails return the default value.
         /// </summary>
@@ -309,6 +487,32 @@ namespace Saritasa.Tools.Common.Utils
             var success = long.TryParse(target, style, provider, out result);
             return success ? result : defaultValue;
         }
+
+#if NETCOREAPP3_0_OR_GREATER || NETSTANDARD2_1_OR_GREATER
+        /// <summary>
+        /// Tries to convert the target string to Int64. If fails return the default value.
+        /// </summary>
+        public static long ParseOrDefault(ReadOnlySpan<char> target, long defaultValue)
+        {
+            long result;
+            var success = long.TryParse(target, out result);
+            return success ? result : defaultValue;
+        }
+
+        /// <summary>
+        /// Tries to convert the target string to Int64. If fails return the default value.
+        /// </summary>
+        public static long ParseOrDefault(ReadOnlySpan<char> target, NumberStyles style, IFormatProvider provider, long defaultValue)
+        {
+            long result;
+            var success = long.TryParse(target, style, provider, out result);
+            return success ? result : defaultValue;
+        }
+#endif
+
+        #endregion
+
+        #region Parse or default sbyte
 
         /// <summary>
         /// Tries to convert the target string to SByte. If fails return the default value.
@@ -332,6 +536,34 @@ namespace Saritasa.Tools.Common.Utils
             return success ? result : defaultValue;
         }
 
+#if NETCOREAPP3_0_OR_GREATER || NETSTANDARD2_1_OR_GREATER
+        /// <summary>
+        /// Tries to convert the target string to SByte. If fails return the default value.
+        /// </summary>
+        [CLSCompliant(false)]
+        public static sbyte ParseOrDefault(ReadOnlySpan<char> target, sbyte defaultValue)
+        {
+            sbyte result;
+            var success = sbyte.TryParse(target, out result);
+            return success ? result : defaultValue;
+        }
+
+        /// <summary>
+        /// Tries to convert the target string to SByte. If fails return the default value.
+        /// </summary>
+        [CLSCompliant(false)]
+        public static sbyte ParseOrDefault(ReadOnlySpan<char> target, NumberStyles style, IFormatProvider provider, sbyte defaultValue)
+        {
+            sbyte result;
+            var success = sbyte.TryParse(target, style, provider, out result);
+            return success ? result : defaultValue;
+        }
+#endif
+
+        #endregion
+
+        #region Parse or default float
+
         /// <summary>
         /// Tries to convert the target string to Single. If fails return the default value.
         /// </summary>
@@ -351,6 +583,32 @@ namespace Saritasa.Tools.Common.Utils
             var success = float.TryParse(target, style, provider, out result);
             return success ? result : defaultValue;
         }
+
+#if NETCOREAPP3_0_OR_GREATER || NETSTANDARD2_1_OR_GREATER
+        /// <summary>
+        /// Tries to convert the target string to Single. If fails return the default value.
+        /// </summary>
+        public static float ParseOrDefault(ReadOnlySpan<char> target, float defaultValue)
+        {
+            float result;
+            var success = float.TryParse(target, out result);
+            return success ? result : defaultValue;
+        }
+
+        /// <summary>
+        /// Tries to convert the target string to Single. If fails return the default value.
+        /// </summary>
+        public static double ParseOrDefault(ReadOnlySpan<char> target, NumberStyles style, IFormatProvider provider, float defaultValue)
+        {
+            float result;
+            var success = float.TryParse(target, style, provider, out result);
+            return success ? result : defaultValue;
+        }
+#endif
+
+        #endregion
+
+        #region Parse or default ushort
 
         /// <summary>
         /// Tries to convert the target string to UInt16. If fails return the default value.
@@ -374,6 +632,34 @@ namespace Saritasa.Tools.Common.Utils
             return success ? result : defaultValue;
         }
 
+#if NETCOREAPP3_0_OR_GREATER || NETSTANDARD2_1_OR_GREATER
+        /// <summary>
+        /// Tries to convert the target string to UInt16. If fails return the default value.
+        /// </summary>
+        [CLSCompliant(false)]
+        public static ushort ParseOrDefault(ReadOnlySpan<char> target, ushort defaultValue)
+        {
+            ushort result;
+            var success = ushort.TryParse(target, out result);
+            return success ? result : defaultValue;
+        }
+
+        /// <summary>
+        /// Tries to convert the target string to UInt16. If fails return the default value.
+        /// </summary>
+        [CLSCompliant(false)]
+        public static ushort ParseOrDefault(ReadOnlySpan<char> target, NumberStyles style, IFormatProvider provider, ushort defaultValue)
+        {
+            ushort result;
+            var success = ushort.TryParse(target, style, provider, out result);
+            return success ? result : defaultValue;
+        }
+#endif
+
+        #endregion
+
+        #region Parse or default uint
+
         /// <summary>
         /// Tries to convert the target string to Uint. If fails return the default value.
         /// </summary>
@@ -395,6 +681,34 @@ namespace Saritasa.Tools.Common.Utils
             var success = uint.TryParse(target, style, provider, out result);
             return success ? result : defaultValue;
         }
+
+#if NETCOREAPP3_0_OR_GREATER || NETSTANDARD2_1_OR_GREATER
+        /// <summary>
+        /// Tries to convert the target string to Uint. If fails return the default value.
+        /// </summary>
+        [CLSCompliant(false)]
+        public static uint ParseOrDefault(ReadOnlySpan<char> target, uint defaultValue)
+        {
+            uint result;
+            var success = uint.TryParse(target, out result);
+            return success ? result : defaultValue;
+        }
+
+        /// <summary>
+        /// Tries to convert the target string to Uint. If fails return the default value.
+        /// </summary>
+        [CLSCompliant(false)]
+        public static uint ParseOrDefault(ReadOnlySpan<char> target, NumberStyles style, IFormatProvider provider, uint defaultValue)
+        {
+            uint result;
+            var success = uint.TryParse(target, style, provider, out result);
+            return success ? result : defaultValue;
+        }
+#endif
+
+        #endregion
+
+        #region Parse or default ulong
 
         /// <summary>
         /// Tries to convert the target string to UInt64. If fails return the default value.
@@ -418,6 +732,34 @@ namespace Saritasa.Tools.Common.Utils
             return success ? result : defaultValue;
         }
 
+#if NETCOREAPP3_0_OR_GREATER || NETSTANDARD2_1_OR_GREATER
+        /// <summary>
+        /// Tries to convert the target string to UInt64. If fails return the default value.
+        /// </summary>
+        [CLSCompliant(false)]
+        public static ulong ParseOrDefault(ReadOnlySpan<char> target, ulong defaultValue)
+        {
+            ulong result;
+            var success = ulong.TryParse(target, out result);
+            return success ? result : defaultValue;
+        }
+
+        /// <summary>
+        /// Tries to convert the target string to UInt64. If fails return the default value.
+        /// </summary>
+        [CLSCompliant(false)]
+        public static ulong ParseOrDefault(ReadOnlySpan<char> target, NumberStyles style, IFormatProvider provider, ulong defaultValue)
+        {
+            ulong result;
+            var success = ulong.TryParse(target, style, provider, out result);
+            return success ? result : defaultValue;
+        }
+#endif
+
+        #endregion
+
+        #region Parse or default enum
+
         /// <summary>
         /// Tries to convert the target string to Enum. If fails return the default value.
         /// </summary>
@@ -433,6 +775,24 @@ namespace Saritasa.Tools.Common.Utils
         {
             return Enum.IsDefined(typeof(T), target) ? (T)Enum.Parse(typeof(T), target, ignoreCase) : defaultValue;
         }
+
+#if NET6_0_OR_GREATER
+        /// <summary>
+        /// Tries to convert the target string to Enum. If fails return the default value.
+        /// </summary>
+        public static T ParseOrDefault<T>(ReadOnlySpan<char> target, T defaultValue) where T : Enum
+        {
+            return Enum.IsDefined(typeof(T), target.ToString()) ? (T)Enum.Parse(typeof(T), target, true) : defaultValue;
+        }
+
+        /// <summary>
+        /// Tries to convert the target string to Enum. If fails return the default value.
+        /// </summary>
+        public static T ParseOrDefault<T>(ReadOnlySpan<char> target, bool ignoreCase, T defaultValue) where T : Enum
+        {
+            return Enum.IsDefined(typeof(T), target.ToString()) ? (T)Enum.Parse(typeof(T), target, ignoreCase) : defaultValue;
+        }
+#endif
 
         #endregion
     }
