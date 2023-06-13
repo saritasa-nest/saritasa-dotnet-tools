3f16ae08ad84d93362ed9afe5cd641dc7884939c
Nikita Epishev <nikita.epishev@saritasa.com>
2023-06-13 14:25:37 +0300

    Remove solutionDir argument SN-168

--- scripts/Modules/Saritasa.Cake/Context/CommonContext.cs
+++ scripts/Modules/Saritasa.Cake/Context/CommonContext.cs
@@ -1,5 +1,4 @@
 ﻿using System.IO;
-using Cake.Common;
 using Cake.Core;
 using Cake.Frosting;
 
@@ -13,19 +12,11 @@ public class CommonContext : FrostingContext
     /// <summary>
     /// Path to solution directory.
     /// </summary>
-    /// <remarks>
-    /// Relative path built from "Saritasa.Cake.csproj" folder.
-    /// DefaultValue - "..\\..\\..\\".
-    /// Was override if invoke "Saritasa.Cake.ps1" with "solutionDir" argument (.\Saritasa.Cake.ps1 --solutionDir ..\\SolutionFolder\\).
-    /// </remarks>
-    public string SolutionDir { get; set; }
+    public readonly string SolutionDir = Path.Combine("..", "..", "..");
 
     /// <summary>
     /// Constructor.
     /// </summary>
     /// <param name="context">Cake context.</param>
-    public CommonContext(ICakeContext context) : base(context)
-    {
-        SolutionDir = context.Argument("solutionDir", Path.Combine("..", "..", ".."));
-    }
+    public CommonContext(ICakeContext context) : base(context) { }
 }
