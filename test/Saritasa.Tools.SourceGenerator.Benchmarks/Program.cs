4060b5f9559de8314c94a4b6adfc7498816e6fec
Danil Morikov <danil.morikov@saritasa.com>
2023-08-17 14:58:53 +0500

    Introduce debug mode and benchmark source generator app SN-140

---
+++ test/Saritasa.Tools.SourceGenerator.Benchmarks/Program.cs
@@ -0,0 +1,93 @@
+﻿using System.ComponentModel;
+
+namespace Saritasa.Tools.SourceGenerator.Benchmark;
+
+/// <summary>
+/// Editable model.
+/// </summary>
+public abstract class EditableModel : INotifyPropertyChanged, INotifyPropertyChanging
+{
+    /// <inheritdoc/>
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    /// <inheritdoc/>
+    public event PropertyChangingEventHandler? PropertyChanging;
+
+    private string hiddenField;
+
+    /// <summary>
+    /// Ignored public mock field.
+    /// </summary>
+    [DoNotNotify]
+    public string baseIgnoredField;
+
+    protected string protectedField;
+
+    /// <summary>
+    /// Public mock field.
+    /// </summary>
+    [AlsoNotify(nameof(baseReflectedField))]
+    public string publicField;
+
+    public string baseReflectedField;
+}
+
+/// <summary>
+/// Mock view model.
+/// </summary>
+public partial class ViewModel : EditableModel
+{
+    private int implementedField;
+
+    /// <summary>
+    /// Implemented mock field.
+    /// </summary>
+    public int ImplementedField
+    {
+        get => implementedField;
+        set => implementedField = value;
+    }
+
+    [AlsoNotify(nameof(reflectedField))]
+    private string field;
+
+    private string reflectedField;
+
+    [DoNotNotify]
+    private string ignoreField;
+
+    private int? nullableField;
+}
+
+/// <summary>
+/// Application point of entry.
+/// </summary>
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("App started.");
+
+        try
+        {
+            var vm = new ViewModel();
+
+            vm.PropertyChanging += Vm_PropertyChanging;
+            vm.PropertyChanged += Vm_PropertyChanged;
+        }
+        finally
+        {
+            Console.ReadLine();
+        }
+    }
+
+    private static void Vm_PropertyChanging(object? sender, PropertyChangingEventArgs e)
+    {
+        // Property changing.
+    }
+
+    private static void Vm_PropertyChanged(object? sender, PropertyChangedEventArgs e)
+    {
+        // Property changed.
+    }
+}
