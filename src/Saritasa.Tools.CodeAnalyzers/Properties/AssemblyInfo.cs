8fab0329678f2adbfcca689d2b6ecdf6536e88a9
Anastasia Lyutova <anastasia.lyutova@saritasa.com>
2023-11-29 13:14:25 +0700

    Add AssemblyInfo file SN-449

---
+++ src/Saritasa.Tools.CodeAnalyzers/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Saritasa.Tools.CodeAnalyzer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Saritasa")]
+[assembly: AssemblyProduct("Saritasa.Tools.CodeAnalyzer")]
+[assembly: AssemblyCopyright("Copyright © Saritasa 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7c9ef279-9821-41ef-8d0c-856f08c1f5cd")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
