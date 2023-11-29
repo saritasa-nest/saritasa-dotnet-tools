f2d04e10b5642e1555130fb6ea0043a16fc2303d
Anastasia Lyutova <anastasia.lyutova@saritasa.com>
2023-11-29 13:16:13 +0700

    Add request handlers analyzer tests SN-449

---
+++ test/Saritasa.Tools.CodeAnalyzers.Tests/RequestHandlersAnalyzer/RequestHandlersAnalyzerTests.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VerifyCS = Saritasa.Tools.CodeAnalyzers.Tests.Verifiers.CSharpCodeFixVerifier<
+    Saritasa.Tools.CodeAnalyzers.Analyzers.RequestHandlersAnalyzer,
+    Saritasa.Tools.CodeAnalyzers.CodeFixProviders.RequestHandlersCodeFixProvider>;
+
+namespace Saritasa.Tools.CodeAnalyzers.Tests.RequestHandlersAnalyzer
+{
+    /// <summary>
+    /// Request handlers analyzer tests.
+    /// </summary>
+    [TestClass]
+    public class RequestHandlersAnalyzerTests
+    {
+        [TestMethod]
+        public async Task DiagnosticCode_ClassWithRequestHandlerWithoutReturnType_DiagnosticWarning()
+        {
+            var diagnosticArgument = "TestRequestHandler";
+            var sourceCode = $@"
+                    using System;
+                    using MediatR;
+                    using System.Threading;
+                    using System.Threading.Tasks;
+
+                    namespace TestApplication
+                    {{
+                        class TestRequest : IRequest {{ }}
+                        class {diagnosticArgument} : IRequestHandler<TestRequest>
+                        {{
+                            public Task Handle(TestRequest request, CancellationToken cancellationToken)
+                                 => throw new NotImplementedException();
+                        }}
+                    }}";
+
+            await new RequestHandlersAnalyzerTestsHelper
+            {
+                TestCode = sourceCode,
+                ExpectedDiagnostics =
+                {
+                    VerifyCS.Diagnostic(Analyzers.RequestHandlersAnalyzer.DiagnosticId)
+                        .WithSeverity(Microsoft.CodeAnalysis.DiagnosticSeverity.Warning)
+                        .WithSpan(10, 31, 10, 49)
+                        .WithArguments(diagnosticArgument)
+                }
+            }.RunAsync();
+        }
+
+        [TestMethod]
+        public async Task DiagnosticCode_ClassWithRequestHandlerAndReturnType_NoDiagnostic()
+        {
+            const string sourceCode = @"
+                    using System;
+                    using MediatR;
+                    using System.Threading;
+                    using System.Threading.Tasks;
+
+                    namespace TestApplication
+                    {
+                        class TestRequest : IRequest<int> { }
+                        class TestRequestHandler : IRequestHandler<TestRequest, int>
+                        {
+                            public Task<int> Handle(TestRequest request, CancellationToken cancellationToken)
+                                 => throw new NotImplementedException();
+                        }
+                    }";
+
+            await new RequestHandlersAnalyzerTestsHelper
+            {
+                TestCode = sourceCode
+            }.RunAsync();
+        }
+
+        [TestMethod]
+        public async Task DiagnosticCode_ClassWithoutRequestHandler_NoDiagnostic()
+        {
+            const string sourceCode = @"
+                    namespace TestApplication
+                    {
+                        class TestClass
+                        {
+                        }
+                    }";
+
+            await new RequestHandlersAnalyzerTestsHelper
+            {
+                TestCode = sourceCode
+            }.RunAsync();
+        }
+    }
+}
