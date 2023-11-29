ee2a15b736f6690920279cbc8f4231d15fe0eecf
Anastasia Lyutova <anastasia.lyutova@saritasa.com>
2023-11-29 13:12:10 +0700

    Add request handlers code fix provider SN-449

---
+++ src/Saritasa.Tools.CodeAnalyzers/CodeFixProviders/RequestHandlersCodeFixProvider.cs
@@ -0,0 +1,33 @@
+using System.Collections.Immutable;
+using System.Composition;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Saritasa.Tools.CodeAnalyzers.Analyzers;
+
+namespace Saritasa.Tools.CodeAnalyzers.CodeFixProviders
+{
+    /// <summary>
+    /// Code fix provider for <see cref="RequestHandlersAnalyzer"/>.
+    /// </summary>
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(RequestHandlersCodeFixProvider))]
+    [Shared]
+    public class RequestHandlersCodeFixProvider : CodeFixProvider
+    {
+        /// <inheritdoc />
+        public sealed override ImmutableArray<string> FixableDiagnosticIds => ImmutableArray<string>.Empty;
+
+        /// <inheritdoc />
+        public sealed override FixAllProvider GetFixAllProvider()
+        {
+            return WellKnownFixAllProviders.BatchFixer;
+        }
+
+        /// <inheritdoc />
+        public sealed override Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            // At the moment we are not implementing a fix code for the analyzer.
+            return Task.CompletedTask;
+        }
+    }
+}
