bbfa02ad17683b5534a0048e1a7a9c6fb8854516
Danil Morikov <danyducky@Danils-MBP.routers.saritasa.io>
2024-02-22 16:53:15 +0300

    Provide accessibility properties SN-140

--- src/Saritasa.Tools.PropertyChangedGenerator/Models/Analyzers/FieldAnalysis.cs
+++ src/Saritasa.Tools.PropertyChangedGenerator/Models/Analyzers/FieldAnalysis.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel;
+using Microsoft.CodeAnalysis;
 
 namespace Saritasa.Tools.PropertyChangedGenerator.Models.Analyzers;
 
@@ -11,6 +12,16 @@ public record FieldAnalysis : MemberAnalysis
     /// Should ignore <see cref="INotifyPropertyChanged.PropertyChanged"/> firing.
     /// </summary>
     public bool DoNotNotify { get; set; }
+
+    /// <summary>
+    /// Getter accessibility.
+    /// </summary>
+    public Accessibility? GetterAccessibility { get; set; }
+
+    /// <summary>
+    /// Setter accessibility.
+    /// </summary>
+    public Accessibility? SetterAccessibility { get; set; }
 
     /// <summary>
     /// Also notify members.
