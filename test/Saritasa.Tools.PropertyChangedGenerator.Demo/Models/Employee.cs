c7b9f36269809fd63b8233dd08a9d5bf56881fe0
danyducky <morikov2000@gmail.com>
2024-03-22 18:56:04 +0300

    Add demo property attributes SN-140

--- test/Saritasa.Tools.PropertyChangedGenerator.Demo/Models/Employee.cs
+++ test/Saritasa.Tools.PropertyChangedGenerator.Demo/Models/Employee.cs
@@ -1,19 +1,27 @@
-﻿namespace Saritasa.Tools.PropertyChangedGenerator.Demo.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Saritasa.Tools.PropertyChangedGenerator.Demo.Models;
 
 /// <summary>
 /// Employee model.
 /// </summary>
 internal partial class Employee : EditableModel
 {
-    [AlsoNotify(nameof(FullName))]
+    [AlsoNotify(nameof(FullName), nameof(Surname))]
+    [Property<RequiredAttribute>]
+    [Property<MinLengthAttribute>(3)]
     private string firstname;
 
     [AlsoNotify(nameof(FullName))]
+    [Property<RequiredAttribute>]
     private string surname;
 
     [Accessibility(Setter.Internal)]
+    [Property<RequiredAttribute>]
+    [Property<RangeAttribute>(0, 100)]
     private int age;
 
+    [Property<RequiredAttribute>]
     private DateTime employedAt;
 
     [DoNotNotify]
@@ -35,6 +43,8 @@ internal partial class Employee : EditableModel
         Firstname = firstname;
         Surname = surname;
         Age = age;
+
+        Attribute attr = new RangeAttribute(0, 100);
 
         EmployedAt = DateTime.Now;
     }
