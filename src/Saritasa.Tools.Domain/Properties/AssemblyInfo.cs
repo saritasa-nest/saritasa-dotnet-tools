4825642b44a95eae74f626b30e83ccea3e9e1a70
Alexander Drozdov <alexander.drozdov@saritasa.com>
2025-06-18 15:41:28 +0700

    Update tags and copyrights SN-893

--- src/Saritasa.Tools.Domain/Properties/AssemblyInfo.cs
+++ src/Saritasa.Tools.Domain/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Saritasa.Tools.Domain")]
@@ -7,7 +6,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Saritasa")]
 [assembly: AssemblyProduct("Saritasa.Tools.Domain")]
-[assembly: AssemblyCopyright("Copyright © Saritasa 2016-2023")]
+[assembly: AssemblyCopyright("Copyright © Saritasa 2016-2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
