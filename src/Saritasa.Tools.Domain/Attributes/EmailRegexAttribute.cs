287c69aa58e4fc9cfde6fd0bccc007614bb2c53b
Alexander Drozdov <alexander.drozdov@saritasa.com>
2025-06-24 19:21:06 +0700

    Add email regex attribute SN-893

---
+++ src/Saritasa.Tools.Domain/Attributes/EmailRegexAttribute.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+using Saritasa.Tools.Domain.Properties;
+
+namespace Saritasa.Tools.Domain.Attributes;
+
+/// <summary>
+/// Required attribute with default message.
+/// </summary>
+[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter, AllowMultiple = false)]
+public class EmailRegexAttribute : DataTypeAttribute
+{
+    private const string EmailValidFormat = @"^[a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9-]+\.[a-zA-Z]{2,}$";
+
+    /// <summary>
+    /// Constructor.
+    /// </summary>
+    public EmailRegexAttribute()
+        : base(DataType.EmailAddress)
+    {
+        ErrorMessageResourceType = typeof(Strings);
+        ErrorMessageResourceName = nameof(Strings.EmailRegexAttribute_Invalid);
+    }
+
+    /// <inheritdoc/>
+    public override bool IsValid(object? value)
+    {
+        if (value is null)
+        {
+            return false;
+        }
+
+        if (value is string valueAsString)
+        {
+            return Regex.IsMatch(valueAsString, EmailValidFormat);
+        }
+
+        return false;
+    }
+}
