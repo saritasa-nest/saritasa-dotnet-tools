1a56ae226d291686c8d58e282bd00fc54d5c1777
Alexander Drozdov <alexander.drozdov@saritasa.com>
2025-07-02 16:39:43 +0700

    Sanitize code SN-893

--- src/Saritasa.Tools.Domain/Localization/SystemResourceLocalizer.cs
+++ src/Saritasa.Tools.Domain/Localization/SystemResourceLocalizer.cs
@@ -6,13 +6,11 @@ namespace Saritasa.Tools.Domain.Localization;
 /// <summary>
 /// Localization provider for non-localized system resources.
 /// </summary>
-public class SystemResourceLocalizer
+public static class SystemResourceLocalizer
 {
-    private readonly FieldInfo resourceManagerField;
-    private readonly Type defaultResourceSource;
-
     /// <summary>
     /// Override System.ComponentModel.Annotations to use alternative ResourceManager.
+    /// Replace embedded .resources.dll file with the custom resource provider.
     /// </summary>
     /// <remarks>
     /// Without this code, no matter what CurrentUICulture is set to, you'll always get English in DataAnnotations validation messages.
@@ -24,7 +22,11 @@ public class SystemResourceLocalizer
     /// Link to a discussion <see href="https://github.com/dotnet/aspnetcore/issues/4848#issuecomment-1925245847."/>.
     /// </para>
     /// </remarks>
-    public static SystemResourceLocalizer ComponentModelAnnotations()
+    /// <param name="resourceSource">
+    /// Resource provider type.
+    /// Default value is provided by Saritasa.Tools.Domain.resources.dll.
+    /// </param>
+    public static void OverrideComponentModelAnnotations(Type? resourceSource = null)
     {
         // Ensure overridable assembly is loaded.
         var test = typeof(global::System.ComponentModel.DataAnnotations.RequiredAttribute);
@@ -35,32 +37,7 @@ public class SystemResourceLocalizer
         // Get overridable resource manager field setter.
         var resourceManagerField = srType!.GetField("s_resourceManager", BindingFlags.Static | BindingFlags.NonPublic)!;
 
-        return new(resourceManagerField, typeof(System.ComponentModel.Annotations.Strings));
-    }
-
-    private SystemResourceLocalizer(
-        FieldInfo resourceManagerField,
-        Type defaultResourceSourceOverride)
-    {
-        this.resourceManagerField = resourceManagerField;
-        this.defaultResourceSource = defaultResourceSourceOverride;
-    }
-
-    /// <summary>
-    /// Replace embedded .resources.dll file with the custom resource provider.
-    /// </summary>
-    /// <param name="resourceSource">
-    /// Resource provider type.
-    /// Default value is provided by Saritasa.Tools.Domain.resources.dll.
-    /// </param>
-    public void OverrideResourceManager(Type? resourceSource = null)
-    {
-        var resourceManager = GetNewResourceManager(resourceSource ?? defaultResourceSource);
+        var resourceManager = new ResourceManager(resourceSource ?? typeof(System.ComponentModel.Annotations.Strings));
         resourceManagerField.SetValue(null, resourceManager);
-    }
-
-    private static ResourceManager GetNewResourceManager(Type resourceSource)
-    {
-        return new ResourceManager($"{resourceSource.Namespace}.Strings", resourceSource.Assembly);
     }
 }
