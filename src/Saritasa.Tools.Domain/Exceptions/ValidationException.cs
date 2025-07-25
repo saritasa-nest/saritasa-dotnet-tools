0e591cd33a04b418daf6a1fbe9c32718df2f46b2
Alexander Drozdov <alexander.drozdov@saritasa.com>
2025-07-25 16:01:57 +0700

    Translate title text for ValidationException when it is used with localizable constructor SN-893

--- src/Saritasa.Tools.Domain/Exceptions/ValidationException.cs
+++ src/Saritasa.Tools.Domain/Exceptions/ValidationException.cs
@@ -189,7 +189,7 @@ public class ValidationException : DomainException
     /// </summary>
     /// <param name="errors">Member error dictionary.</param>
     public ValidationException(IDictionary<string, FormattedString> errors) :
-        base(DomainErrorDescriber.Default.ValidationErrors())
+        base(FormattedString.FromResGen(DomainErrorDescriber.Default.ValidationErrors))
     {
         if (errors == null)
         {
@@ -222,7 +222,7 @@ public class ValidationException : DomainException
     /// </summary>
     /// <param name="errors">Member errors dictionary.</param>
     public ValidationException(IDictionary<string, ICollection<FormattedString>> errors) :
-        base(DomainErrorDescriber.Default.ValidationErrors())
+        base(FormattedString.FromResGen(DomainErrorDescriber.Default.ValidationErrors))
     {
         if (errors == null)
         {
@@ -252,7 +252,7 @@ public class ValidationException : DomainException
     /// </summary>
     /// <param name="errors">Member errors dictionary.</param>
     public ValidationException(IDictionary<string, IEnumerable<FormattedString>> errors) :
-        base(DomainErrorDescriber.Default.ValidationErrors())
+        base(FormattedString.FromResGen(DomainErrorDescriber.Default.ValidationErrors))
     {
         if (errors == null)
         {
