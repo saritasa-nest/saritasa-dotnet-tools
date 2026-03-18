a1c7b5c6f53e01182403a2311400af0c50501d59
Alexandr Makarov <Alexandr.Makarov@saritasa.com>
2026-03-18 10:22:08 +0700

    Change tests order SN-927

--- test/Saritasa.Tools.CodeAnalyzers.Tests/ExceptionMessageDotAnalyzerTests/ExceptionMessageDotAnalyzerTests.cs
+++ test/Saritasa.Tools.CodeAnalyzers.Tests/ExceptionMessageDotAnalyzerTests/ExceptionMessageDotAnalyzerTests.cs
@@ -54,6 +54,35 @@ public class ExceptionMessageDotAnalyzerTests
     }
 
     /// <summary>
+    /// Validates that an exception message wit a dot does not produce a warning.
+    /// </summary>
+    [TestMethod]
+    public async Task ExceptionMessage_WithDot_ShouldNotProduceWarning()
+    {
+        const string sourceCode =
+            /* lang=c# */
+            """
+            using System;
+            using System.Threading;
+            using System.Threading.Tasks;
+
+            namespace TestApplication
+            {
+                class TestClass
+                {
+                    public void TestMethod()
+                    {
+                        throw new ArgumentException("This is an error message with a dot.");
+                    }
+                }
+            }
+            """;
+
+        context.TestCode = sourceCode;
+        await context.RunAsync();
+    }
+
+    /// <summary>
     /// Validates that local constant without a dot produces a warning.
     /// </summary>
     [TestMethod]
@@ -207,6 +236,37 @@ public class ExceptionMessageDotAnalyzerTests
     }
 
     /// <summary>
+    /// Validates that an interpolated exception message with a dot does not produce a warning.
+    /// </summary>
+    [TestMethod]
+    public async Task ExceptionMessage_Interpolated_WithDot_ShouldNotProduceWarning()
+    {
+        const string sourceCode =
+            /* lang=c# */
+            """
+            using System;
+            using System.Threading;
+            using System.Threading.Tasks;
+
+            namespace TestApplication
+            {
+                class TestClass
+                {
+                    public void TestMethod()
+                    {
+                        const string test = "test";
+                        const string test2 = "test2.";
+                        throw new ArgumentException($"{test} {test2}");
+                    }
+                }
+            }
+            """;
+
+        context.TestCode = sourceCode;
+        await context.RunAsync();
+    }
+
+    /// <summary>
     /// Validates that a binary operation in exception message without a dot produces a warning.
     /// </summary>
     [TestMethod]
@@ -237,66 +297,6 @@ public class ExceptionMessageDotAnalyzerTests
     }
 
     /// <summary>
-    /// Validates that an exception message wit a dot does not produce a warning.
-    /// </summary>
-    [TestMethod]
-    public async Task ExceptionMessage_WithDot_ShouldNotProduceWarning()
-    {
-        const string sourceCode =
-            /* lang=c# */
-            """
-            using System;
-            using System.Threading;
-            using System.Threading.Tasks;
-
-            namespace TestApplication
-            {
-                class TestClass
-                {
-                    public void TestMethod()
-                    {
-                        throw new ArgumentException("This is an error message with a dot.");
-                    }
-                }
-            }
-            """;
-
-        context.TestCode = sourceCode;
-        await context.RunAsync();
-    }
-
-    /// <summary>
-    /// Validates that an interpolated exception message with a dot does not produce a warning.
-    /// </summary>
-    [TestMethod]
-    public async Task ExceptionMessage_Interpolated_WithDot_ShouldNotProduceWarning()
-    {
-        const string sourceCode =
-            /* lang=c# */
-            """
-            using System;
-            using System.Threading;
-            using System.Threading.Tasks;
-
-            namespace TestApplication
-            {
-                class TestClass
-                {
-                    public void TestMethod()
-                    {
-                        const string test = "test";
-                        const string test2 = ".";
-                        throw new ArgumentException($"{test} {test2}");
-                    }
-                }
-            }
-            """;
-
-        context.TestCode = sourceCode;
-        await context.RunAsync();
-    }
-
-    /// <summary>
     /// Validates that a binary operation in exception message with a dot does not produce a warning.
     /// </summary>
     [TestMethod]
@@ -316,7 +316,7 @@ public class ExceptionMessageDotAnalyzerTests
                     public void TestMethod()
                     {
                         var error = "Error";
-                        throw new ArgumentException(error + ".");
+                        throw new ArgumentException(error + "test.");
                     }
                 }
             }
