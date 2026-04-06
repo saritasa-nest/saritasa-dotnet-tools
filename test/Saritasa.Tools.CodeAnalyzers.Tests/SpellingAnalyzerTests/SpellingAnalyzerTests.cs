5a35dfd60209c46f8b8986ad01bdcfd082042104
Alexandr Makarov <Alexandr.Makarov@saritasa.com>
2026-04-06 14:38:57 +0700

    Improve GUID tests SN-933

--- test/Saritasa.Tools.CodeAnalyzers.Tests/SpellingAnalyzerTests/SpellingAnalyzerTests.cs
+++ test/Saritasa.Tools.CodeAnalyzers.Tests/SpellingAnalyzerTests/SpellingAnalyzerTests.cs
@@ -695,7 +695,7 @@ public class SpellingAnalyzerTests
     /// Verifies <see cref="Guid"/> string does not produce a warning.
     /// </summary>
     [TestMethod]
-    public async Task Guid_ShouldNotProduceWarning()
+    public async Task Guid_String_ShouldNotProduceWarning()
     {
         context.TestCode =
             /* lang=c# */
@@ -706,7 +706,31 @@ public class SpellingAnalyzerTests
                 {
                     public void TestMethod()
                     {
-                        var test = "This is GUID 1b6cdb5b-8449-4d8e-ad3b-6b3dd8f4158d";
+                        var test = "This GUID 1b6cdb5b-8449-4d8e-ad3b-6b3dd8f4158d should not be spellchecked";
+                    }
+                }
+            }
+            """;
+
+        await context.RunAsync();
+    }
+
+    /// <summary>
+    /// Verifies <see cref="Guid"/> comment does not produce a warning.
+    /// </summary>
+    [TestMethod]
+    public async Task Guid_Comment_ShouldNotProduceWarning()
+    {
+        context.TestCode =
+            /* lang=c# */
+            """
+            namespace TestApplication
+            {
+                class TestClass
+                {
+                    public void TestMethod()
+                    {
+                        // This GUID 1b6cdb5b-8449-4d8e-ad3b-6b3dd8f4158d should not be spellchecked
                     }
                 }
             }
